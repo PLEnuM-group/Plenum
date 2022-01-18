d69c9529dab4d8d8803493b3195f3826088e78a5
Lisa Schumacher <lj.schumacher@tum.de>
2022-01-18 16:13:15 +0100

    up up

--- core/aeff_calculations.py
+++ core/aeff_calculations.py
@@ -5,124 +5,135 @@ import pickle
 import astropy.units as u
 from astropy.time import Time
 from astropy.coordinates import SkyCoord, EarthLocation, AltAz
-from scipy.interpolate import RegularGridInterpolator, splrep, splev
+from scipy.interpolate import RegularGridInterpolator
 from scipy.stats import gaussian_kde
 from settings import LIVETIME, GAMMA_ASTRO, PHI_ASTRO, poles
 from tools import get_mids
 
 # with three dimensions: sindec, energy, ra
 def aeff_eval(aeff, sindec_width, e_width, ra_width):
-    return ((aeff * sindec_width).T * e_width)[:,:,np.newaxis] * np.atleast_2d(ra_width)
+    return ((aeff * sindec_width).T * e_width)[:, :, np.newaxis] * np.atleast_2d(
+        ra_width
+    )
+
 
 # with two dimensions: sindec, energy
 def aeff_eval_e_sd(aeff, sindec_width, e_width, ra_width):
-    return (aeff * sindec_width).T * e_width * np.sum(ra_width) # = 2pi
+    return (aeff * sindec_width).T * e_width * np.sum(ra_width)  # = 2pi
+
 
 def setup_aeff_grid(aeff_baseline, sindec_mids, ra_mids, ra_width):
     # Interpolated grid of the effective area in "local" coordinates
     # (= icecube's native coordinates)
-    grid2d = [RegularGridInterpolator(
-        (np.arcsin(sindec_mids), ra_mids), # transform dec to local theta
-        # switch back to local zenith, add ra as new axis and normalize accordingly
-        aeff_baseline[i][::-1,np.newaxis] / np.atleast_2d(ra_width)/ len(ra_mids),
-        method='linear',
-        bounds_error=False,
-        fill_value=0.
-    ) for i in range(len(aeff_baseline))]
+    grid2d = [
+        RegularGridInterpolator(
+            (np.arcsin(sindec_mids), ra_mids),  # transform dec to local theta
+            # switch back to local zenith, add ra as new axis and normalize accordingly
+            aeff_baseline[i][::-1, np.newaxis] / np.atleast_2d(ra_width) / len(ra_mids),
+            method="linear",
+            bounds_error=False,
+            fill_value=0.0,
+        )
+        for i in range(len(aeff_baseline))
+    ]
     # grid elements are calculated for each energy bin, grid is theta x phi
     # coordinate grid in equatorial coordinates (icrs)
     # these will be the integration coordinates
     pp, tt = np.meshgrid(ra_mids, np.arcsin(sindec_mids))
-    eq_coords = SkyCoord(
-        pp * u.radian,
-        tt * u.radian,
-        frame="icrs"
-    )
+    eq_coords = SkyCoord(pp * u.radian, tt * u.radian, frame="icrs")
     return grid2d, eq_coords
 
+
 def aeff_rotation(coord_lat, coord_lon, eq_coords, grid2d, ra_width):
-    '''
+    """
     ## Idea: transform the integration over R.A. per sin(dec) into local coordinates
-    # **Note:** here, equal contributions of each detector are assumed. 
-    # In case one wants to use different livetimes, the effective areas have to multiplied 
+    # **Note:** here, equal contributions of each detector are assumed.
+    # In case one wants to use different livetimes, the effective areas have to multiplied
     # individually before calculating e.g. the expected number of astrophysical events
-    '''
+    """
     # local detector
-    loc = EarthLocation(
-        lat=coord_lat,
-        lon=coord_lon
-    )
+    loc = EarthLocation(lat=coord_lat, lon=coord_lon)
     # arbitrary time, doesnt matter here
-    time = Time('2021-6-21 00:00:00')
+    time = Time("2021-6-21 00:00:00")
     # transform integration coordinates to local frame
     local_coords = eq_coords.transform_to(AltAz(obstime=time, location=loc))
-    # sum up the contributions over the transformed RA axis per declination 
+    # sum up the contributions over the transformed RA axis per declination
     # loop over the energy bins to get the same shape of aeff as before
     # sum along transformed ra coordinates
-    return np.array([np.sum(
-        grid2d[i]((local_coords.alt.rad, local_coords.az.rad)) * ra_width, # integrate over RA
-        axis=1) for i in range(len(grid2d))])
+    return np.array(
+        [
+            np.sum(
+                grid2d[i]((local_coords.alt.rad, local_coords.az.rad))
+                * ra_width,  # integrate over RA
+                axis=1,
+            )
+            for i in range(len(grid2d))
+        ]
+    )
+
 
 def get_aeff_and_binnings(key="full", verbose=False):
-    ''' key: "full" for full effective area
-             "upgoing" for effective area >-5deg
-    '''
+    """key: "full" for full effective area
+    "upgoing" for effective area >-5deg
+    """
     with open(f"../resources/tabulated_logE_sindec_aeff_{key}.pckl", "rb") as f:
         log_ebins, sindec_bins, aeff_2d = pickle.load(f)
     ebins = np.power(10, log_ebins)
-    ra_bins = np.linspace(0, np.pi*2, num=101)
+    ra_bins = np.linspace(0, np.pi * 2, num=101)
     if verbose:
         print(len(ebins) - 1, "log_10(energy) bins")
         print(len(sindec_bins) - 1, "declination bins")
-        print(len(ra_bins) - 1, "RA bins")    
+        print(len(ra_bins) - 1, "RA bins")
     return aeff_2d, log_ebins, ebins, sindec_bins, ra_bins
+
 
 def calc_energy_smearing():
     # Calculate energy smearing
     # this takes a couple of seconds
     public_data_hist = np.genfromtxt(
-        "../icecube_10year_ps/irfs/IC86_II_smearing.csv",
-        skip_header=1
+        "../icecube_10year_ps/irfs/IC86_II_smearing.csv", skip_header=1
     )
-    log_sm_emids = (public_data_hist[:,0] + public_data_hist[:,1]) / 2.
-    log_sm_ereco_mids = (public_data_hist[:,4] + public_data_hist[:,5]) / 2.
-    fractional_event_counts = public_data_hist[:,10]
+    log_sm_emids = (public_data_hist[:, 0] + public_data_hist[:, 1]) / 2.0
+    log_sm_ereco_mids = (public_data_hist[:, 4] + public_data_hist[:, 5]) / 2.0
+    fractional_event_counts = public_data_hist[:, 10]
 
     eri = get_mids(np.arange(0.5, 9, 0.2))
     log_emids = get_mids(np.log10(ebins))
     ee, rr = np.meshgrid(log_emids, eri)
 
     e_ereco_kdes = gaussian_kde(
-        (log_sm_emids, log_sm_ereco_mids),
-        weights=fractional_event_counts
+        (log_sm_emids, log_sm_ereco_mids), weights=fractional_event_counts
     )
     # has shape ereco x etrue
-    return e_ereco_kdes([ee.flatten(),rr.flatten()]).reshape(len(eri), len(log_emids))
+    return e_ereco_kdes([ee.flatten(), rr.flatten()]).reshape(len(eri), len(log_emids))
+
 
 def energy_smearing(ematrix, ev):
     return (ematrix @ ev.T).T
 
+
 if __name__ == "__main__":
     # get all info from data release first
     d_public = np.genfromtxt(
-        '../icecube_10year_ps/irfs/IC86_II_effectiveArea.csv',
-        skip_header=1
+        "../icecube_10year_ps/irfs/IC86_II_effectiveArea.csv", skip_header=1
     )
     # log10(E_nu/GeV)_min log10(E_nu/GeV)_max
     # Dec_nu_min[deg] Dec_nu_max[deg]
     # A_Eff[cm^2]
     # the file contains all bin edges
-    aeff = d_public[:,4]
-    
-    sindec_bins = np.unique(np.sin(np.deg2rad([d_public[:,2], d_public[:,3]])))
+    aeff = d_public[:, 4]
+
+    sindec_bins = np.unique(np.sin(np.deg2rad([d_public[:, 2], d_public[:, 3]])))
     sindec_mids = get_mids(sindec_bins)
     sindec_width = np.diff(sindec_bins)
 
-    ebins = np.unique(np.power(10, [d_public[:,0], d_public[:,1]])) # all bin edges in order
+    ebins = np.unique(
+        np.power(10, [d_public[:, 0], d_public[:, 1]])
+    )  # all bin edges in order
     emids = get_mids(ebins)
     ewidth = np.diff(ebins)
-    
-    ra_bins = np.linspace(0, np.pi*2, num=101)
+
+    ra_bins = np.linspace(0, np.pi * 2, num=101)
     ra_mids = get_mids(ra_bins)
     ra_width = np.diff(ra_bins)
 
@@ -135,25 +146,28 @@ if __name__ == "__main__":
     min_idx = np.searchsorted(sindec_mids, np.sin(np.deg2rad(-5)))
     print(f"Below {np.rad2deg(np.arcsin(sindec_bins[min_idx])):1.2f}deg aeff is 0")
     aeff_2d["icecube"] = np.copy(aeff_2d["icecube_full"])
-    aeff_2d["icecube"][:,:min_idx] = 0
+    aeff_2d["icecube"][:, :min_idx] = 0
 
     # some event numbers for checking
     det = "icecube"
     aeff_factor = (aeff_2d[det] * sindec_width).T * ewidth * LIVETIME * np.sum(ra_width)
-    astro_ev = aeff_factor * (emids/1E5)**(-GAMMA_ASTRO) * PHI_ASTRO
+    astro_ev = aeff_factor * (emids / 1e5) ** (-GAMMA_ASTRO) * PHI_ASTRO
     print(det)
     print("astro events:", np.sum(astro_ev))
     # should be something like 2300 neutrinos for given parameters
 
     print("starting aeff rotations")
     grid2d, eq_coords = setup_aeff_grid(
-        aeff_2d["icecube"], sindec_mids, ra_mids, ra_width)
+        aeff_2d["icecube"], sindec_mids, ra_mids, ra_width
+    )
     aeff_i = {}
     aeff_i["Plenum-1"] = np.zeros_like(aeff_2d["icecube"])
 
     # loop over detectors
-    for k in ['IceCube', 'P-ONE', 'KM3NeT','Baikal-GVD']:    
-        aeff_i[k] = aeff_rotation(poles[k]["lat"], poles[k]["lon"], eq_coords, grid2d, ra_width)
+    for k in ["IceCube", "P-ONE", "KM3NeT", "Baikal-GVD"]:
+        aeff_i[k] = aeff_rotation(
+            poles[k]["lat"], poles[k]["lon"], eq_coords, grid2d, ra_width
+        )
         aeff_i["Plenum-1"] += aeff_i[k]
 
     ## GEN-2 will have ~7.5x effective area ==> 5times better discovery potential
@@ -167,30 +181,33 @@ if __name__ == "__main__":
     with open(savefile, "wb") as f:
         pickle.dump((np.log10(ebins), sindec_bins, aeff_i), f)
 
-
     # same but wit FULL icecube effective area
     print("starting full effective area calculation...")
     grid2d, eq_coords = setup_aeff_grid(
-        aeff_2d["icecube_full"], sindec_mids, ra_mids, ra_width)
+        aeff_2d["icecube_full"], sindec_mids, ra_mids, ra_width
+    )
 
     aeff_i_full = {}
     aeff_i_full["Plenum-1"] = np.zeros_like(aeff_2d["icecube_full"])
 
     # loop over detectors
-    for k in ['IceCube', 'P-ONE', 'KM3NeT','Baikal-GVD']:    
-        aeff_i_full[k] = aeff_rotation(poles[k]["lat"], poles[k]["lon"], eq_coords, grid2d, ra_width)
+    for k in ["IceCube", "P-ONE", "KM3NeT", "Baikal-GVD"]:
+        aeff_i_full[k] = aeff_rotation(
+            poles[k]["lat"], poles[k]["lon"], eq_coords, grid2d, ra_width
+        )
         aeff_i_full["Plenum-1"] += aeff_i_full[k]
 
     # GEN-2 will have ~7.5x effective area ==> 5times better discovery potential
     aeff_i_full["Gen-2"] = aeff_i_full["IceCube"] * 5 ** (1 / 0.8)
-    aeff_i_full["Plenum-2"] = aeff_i_full["Plenum-1"] - aeff_i_full["IceCube"] + aeff_i_full["Gen-2"]
+    aeff_i_full["Plenum-2"] = (
+        aeff_i_full["Plenum-1"] - aeff_i_full["IceCube"] + aeff_i_full["Gen-2"]
+    )
 
     # save
     savefile = "../resources/tabulated_logE_sindec_aeff_full.pckl"
     print("Saving full effective areas to", savefile)
     with open(savefile, "wb") as f:
         pickle.dump((np.log10(ebins), sindec_bins, aeff_i_full), f)
-
 
     print("Calculate energy smearing...")
     kvals = calc_energy_smearing()
--- core/settings.py
+++ core/settings.py
@@ -1,9 +1,6 @@
-import os
 import numpy as np
-import scipy as sp
-import pickle
 import matplotlib.colors as mc
-from matplotlib.colors import LogNorm
+
 try:
     import colorsys
 except:
@@ -14,7 +11,8 @@ from astropy.coordinates import SkyCoord
 import astropy.units as u
 
 import warnings
-warnings.filterwarnings('ignore')
+
+warnings.filterwarnings("ignore")
 
 matplotlib = sns.mpl
 plt = matplotlib.pyplot
@@ -24,15 +22,15 @@ sns.set(
     font="serif",
     font_scale=2,
     rc={
-        'font.family': 'serif',
-        'lines.linewidth': 2,
-        'axes.grid': True,
-        'grid.color': "0.9",
-        'mathtext.fontset': 'cm',
-        'image.cmap': 'mako',
-        'savefig.format': 'pdf',
-        'savefig.dpi': 200,
-    }
+        "font.family": "serif",
+        "lines.linewidth": 2,
+        "axes.grid": True,
+        "grid.color": "0.9",
+        "mathtext.fontset": "cm",
+        "image.cmap": "mako",
+        "savefig.format": "pdf",
+        "savefig.dpi": 200,
+    },
 )
 sns.set_palette("Set2", n_colors=4)
 
@@ -41,53 +39,87 @@ colors = [
     (0.4287136896080445, 0.8230641296498253, 0.7976237339879146),
     (0.17544617305706944, 0.6466723383995561, 0.6162725242360733),
     (0.024792120980593635, 0.4191425610223744, 0.39370216536046365),
-    (0.713458847396123, 0.261074771588123, 0.29025904200122044)
+    (0.713458847396123, 0.261074771588123, 0.29025904200122044),
 ]
 many_colors = ["0.8", "0.4", "0."]
-many_colors.extend(sns.cubehelix_palette(
-    start=0.9, rot=0, n_colors=3, light=0.8, dark=0.4, hue=1.5))
-many_colors.extend(sns.cubehelix_palette(
-    start=2.4, rot=0, n_colors=3, light=0.8, dark=0.4, hue=1.5))
+many_colors.extend(
+    sns.cubehelix_palette(start=0.9, rot=0, n_colors=3, light=0.8, dark=0.4, hue=1.5)
+)
+many_colors.extend(
+    sns.cubehelix_palette(start=2.4, rot=0, n_colors=3, light=0.8, dark=0.4, hue=1.5)
+)
 many_colors = np.array(many_colors)
 
 # define location of experiments
 # some plot settings
 poles = {
-    'IceCube': {"lon": 1*u.deg, "lat": -90*u.deg, "color": "k", "ls": "-", "label": "IceCube"},
-    'Gen-2': {"lon": 1*u.deg, "lat": -90*u.deg, "color": "gray", "ls": "-.", "label": "IceCube x 7.5"},
-    'P-ONE': {"lon": -123.3656*u.deg, "lat": 48.4284*u.deg, "color": colors[0], "ls": "--", "label": "at P-ONE location"},
-    'KM3NeT': {"lon": (16 + 6/60)*u.deg, "lat": (36 + 16/60)*u.deg, "color": colors[1], "ls": "--", "label": "at KM3NeT location"},
-    'Baikal-GVD': {"lon": 108.1650*u.deg, "lat": 53.5587*u.deg, "color": colors[2], "ls": "--", "label": "at GVD location"},
-    'Plenum-1': {"color": "#a50000", "ls": "-", "label": r"PLE$\nu$M-1"},
-    'Plenum-2': {"color": "#a50000", "ls": "-.", "label": r"PLE$\nu$M-2"},
+    "IceCube": {
+        "lon": 1 * u.deg,
+        "lat": -90 * u.deg,
+        "color": "k",
+        "ls": "-",
+        "label": "IceCube",
+    },
+    "Gen-2": {
+        "lon": 1 * u.deg,
+        "lat": -90 * u.deg,
+        "color": "gray",
+        "ls": "-.",
+        "label": "IceCube x 7.5",
+    },
+    "P-ONE": {
+        "lon": -123.3656 * u.deg,
+        "lat": 48.4284 * u.deg,
+        "color": colors[0],
+        "ls": "--",
+        "label": "at P-ONE location",
+    },
+    "KM3NeT": {
+        "lon": (16 + 6 / 60) * u.deg,
+        "lat": (36 + 16 / 60) * u.deg,
+        "color": colors[1],
+        "ls": "--",
+        "label": "at KM3NeT location",
+    },
+    "Baikal-GVD": {
+        "lon": 108.1650 * u.deg,
+        "lat": 53.5587 * u.deg,
+        "color": colors[2],
+        "ls": "--",
+        "label": "at GVD location",
+    },
+    "Plenum-1": {"color": "#a50000", "ls": "-", "label": r"PLE$\nu$M-1"},
+    "Plenum-2": {"color": "#a50000", "ls": "-.", "label": r"PLE$\nu$M-2"},
 }
 
 
 # aeff in m^2
 # energy in GeV
-LIVETIME = 10 * 3600 * 24 * 365.24 # 10 years in seconds
-E_NORM = 1E5 # normalization energy of power law (E/E_NORM)^gamma
+LIVETIME = 10 * 3600 * 24 * 365.24  # 10 years in seconds
+E_NORM = 1e5  # normalization energy of power law (E/E_NORM)^gamma
 # from phd thesis of Joeran Stettner (IC diffuse benchmark on numu)
 # temporary link: https://www.institut3b.physik.rwth-aachen.de/global/show_document.asp?id=aaaaaaaaawyqakk
 # will be published as paper soon
-#GAMMA_ASTRO = 2.37
-#PHI_ASTRO_FACTOR = 1.36
+# GAMMA_ASTRO = 2.37
+# PHI_ASTRO_FACTOR = 1.36
 
 # his previous PUBLISHED work, ICRC2019: https://arxiv.org/abs/1908.09551
 GAMMA_ASTRO = 2.28
 PHI_ASTRO_FACTOR = 1.44
-PHI_0 = 1.0E-18 # * (E/100 TeV)^gamma / GeV / sr / cm^2 / s
-PHI_ASTRO = PHI_ASTRO_FACTOR * PHI_0 # * (E/100 TeV)^gamma / GeV / sr / cm^2 / s
+PHI_0 = 1.0e-18  # * (E/100 TeV)^gamma / GeV / sr / cm^2 / s
+PHI_ASTRO = PHI_ASTRO_FACTOR * PHI_0  # * (E/100 TeV)^gamma / GeV / sr / cm^2 / s
 
 sgr_a = SkyCoord(0, 0, unit="rad", frame="galactic")
 txs0506 = SkyCoord(77.36, 5.69, unit="deg", frame="icrs")
 ngc1068 = SkyCoord(40.67, -0.01, unit="deg", frame="icrs")
 
+
 def reset_palette(n_colors, pal="crest"):
     sns.set_palette(pal, n_colors=n_colors)
 
+
 def slightly_change_color(color, amount=0.2):
-    """ slightly change the color hue"""
+    """slightly change the color hue"""
     if not colorsys:
         print("Cannot change color.")
         return color
@@ -96,12 +128,12 @@ def slightly_change_color(color, amount=0.2):
     except:
         c = color
     c = colorsys.rgb_to_hls(*mc.to_rgb(c))
-    ld = 0.1 if c[1]<=0.5 else -0.1
-    return colorsys.hls_to_rgb(c[0]+amount, c[1] + ld, c[2])
+    ld = 0.1 if c[1] <= 0.5 else -0.1
+    return colorsys.hls_to_rgb(c[0] + amount, c[1] + ld, c[2])
 
 
 def change_color_ld(color, amount=0.2):
-    """ slightly change the color lightness/darkness"""
+    """slightly change the color lightness/darkness"""
     if not colorsys:
         print("Cannot change color.")
         return color
