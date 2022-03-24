76614d30e1d76173dba667fb1e0bda1a4109f751
Lisa Schumacher <lj.schumacher@tum.de>
2022-03-24 10:09:12 +0100

    updates on PS analysis; updated diffuse contour

--- core/settings.py
+++ core/settings.py
@@ -77,15 +77,15 @@ E_NORM = 1e5  # normalization energy of power law (E/E_NORM)^gamma
 # --> 100 TeV
 # from phd thesis of Joeran Stettner (IC diffuse benchmark on numu)
 # temporary link: https://www.institut3b.physik.rwth-aachen.de/global/show_document.asp?id=aaaaaaaaawyqakk
-# will be published as paper soon
-# GAMMA_ASTRO = 2.37
-# PHI_ASTRO_FACTOR = 1.36
+# UPDATE: paper now available at https://arxiv.org/abs/2111.10299
+GAMMA_ASTRO = 2.37
+PHI_ASTRO_FACTOR = 1.44
 
-# his previous PUBLISHED work, ICRC2019: https://arxiv.org/abs/1908.09551
-GAMMA_ASTRO = 2.28
+# previous PUBLISHED work, ICRC2019: https://arxiv.org/abs/1908.09551
+# GAMMA_ASTRO = 2.28
+
 # we use this factor such that PHI_ASTRO_FACTOR can be of order 1
 PHI_0 = 1.0e-18  # * (E/100 TeV)^gamma / GeV / sr / cm^2 / s
-PHI_ASTRO_FACTOR = 1.44
 PHI_ASTRO = PHI_ASTRO_FACTOR * PHI_0  # * (E/100 TeV)^gamma / GeV / sr / cm^2 / s
 
 sgr_a = SkyCoord(0, 0, unit="rad", frame="galactic")
