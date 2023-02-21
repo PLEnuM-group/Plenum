fixed a dec-sindec bug in aeff rotation

core/aeff_calculations.py
@@ -105,18 +105,11 @@ def setup_aeff_grid(aeff_baseline, sindec_mids, ra_mids, ra_width, local=False):
         + (np.arcsin(sindec_mids)[-1] - np.arcsin(sindec_mids)[-2]),
     )
     ra_endvalues = (ra_mids[0] - ra_width[0], ra_mids[-1] + ra_width[-1])
-    padded_sindec_mids = np.pad(
-        np.arcsin(sindec_mids),
-        pad_width=1,
-        mode="linear_ramp",
-        end_values=sd_endvalues,
+    padded_sindec_mids = np.concatenate(
+        [[sd_endvalues[0]], np.arcsin(sindec_mids), [sd_endvalues[1]]]
     )
-    padded_ra_mids = np.pad(
-        ra_mids,
-        pad_width=1,
-        mode="linear_ramp",
-        end_values=ra_endvalues,
-    )
+    padded_ra_mids = np.concatenate([[ra_endvalues[0]], ra_mids, [ra_endvalues[1]]])
+
     for aeff in aeff_baseline:
 
         if not local:
@@ -134,8 +127,8 @@ def setup_aeff_grid(aeff_baseline, sindec_mids, ra_mids, ra_width, local=False):
                 (padded_sindec_mids, padded_ra_mids),
                 padded_aeff,
                 method="linear",
-                bounds_error=False,
-                fill_value=0.0,
+                bounds_error=True, 
+                fill_value=0.0, # not needed actually, since bounds_error is True
             )
         )
     # grid elements are calculated for each energy bin, grid is theta x phi
@@ -164,7 +157,7 @@ def aeff_rotation(coord_lat, coord_lon, eq_coords, grid2d, ra_width):
     return np.array(
         [
             np.sum(
-                grid2d[i]((local_coords.alt.rad, local_coords.az.rad))
+                grid2d[i]((np.sin(local_coords.alt.rad), local_coords.az.rad))
                 * ra_width,  # integrate over RA
                 axis=1,
             )

core/tools.py
@@ -43,7 +43,10 @@ def poisson_llh(mu_i, k_i):
     L(data k | model mu)  = prod_{i,j} mu_ij ** k_ij / k_ij! * exp(-mu_ij)
 
     For numerical stability, we directly evaluate the log of the poisson probability
-    (see https://en.wikipedia.org/wiki/Stirling%27s_approximation for approximation of the faculty function)
+    (see https://en.wikipedia.org/wiki/Stirling%27s_approximation for approximation of the faculty function).
+
+    Since we are using Asimov data that can have floating point values, we need to implement the function
+    instead of using scipy.stats.poisson.logpmf. (It fails for floats in k_i!!)
 
     -2 log (L) = -2 [k_i log(mu_i) - mu_i - 0.5 log(2 pi k_i) + k_i - k_i log(k_i)]
 