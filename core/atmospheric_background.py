98c8a42427b2d89b3321b8fcc488232402be96e8
Lisa Schumacher <lisa.j.schumacher@fau.de>
2023-11-29 15:33:02 +0100

    cleaning up likelihood code duplications

--- core/atmospheric_background.py
+++ core/atmospheric_background.py
@@ -1,105 +1,31 @@
-from settings import *
+import numpy as np
+from pickle import dump
+from settings import BASEPATH
 from os.path import join
-import mceq_config as config
-from MCEq.core import MCEqRun
-import crflux.models as pm
-import pickle
 import argparse
 
-parser = argparse.ArgumentParser(description="Calculate atmospheric fluxes with MCEq.")
-parser.add_argument("-s", "--savefile", type=str, default="MCEq_flux.pckl")
+from daemonflux import Flux
+
+parser = argparse.ArgumentParser(description="Calculate atmospheric fluxes with MCEq/daemonflux.")
+parser.add_argument("-s", "--savefile", type=str, default="MCEq_daemonflux.pckl")
 args = parser.parse_args()
 savepath = join(BASEPATH, "resources", args.savefile)
 print("Flux will be saved to:", savepath)
 
+# setup daemonflux with IceCube location/atmosphere
+# currently we use this for all detectors, just rotated to the specific latitude
+# TODO: use regular atmosphere for the other detectors
 
-# setup
-config.e_min = 1e-1
-config.e_max = 1e12
+fl = Flux(location="IceCube", use_calibration=True, debug=1)
+egrid = np.logspace(0, 9)
 
-mceq = MCEqRun(
-    interaction_model="SIBYLL2.3c",
-    primary_model=(pm.HillasGaisser2012, "H3a"),
-    theta_deg=0.0,
-    density_model=("MSIS00_IC", ("SouthPole", "January")),
-)
+flux_grid = np.zeros((len(fl.zenith_angles), len(egrid)))
+zeniths = np.array(fl.zenith_angles, dtype=float)
+for ii, zz in enumerate(fl.zenith_angles):
+    # default is flux * E^3, so we need to correct for that
+    flux_grid[ii] = fl.flux(egrid, zz, "total_numuflux")  / (egrid**3)
+flux_grid = flux_grid.T
+savepath = join(BASEPATH, "resources", "MCEq_daemonflux.pckl")
 
-
-mag = 0
-# Define equidistant grid in cos(theta)
-angles = np.rad2deg(np.arccos(np.linspace(1, -1, 21)))
-flux_def = dict()
-
-all_component_names = [
-    "numu_conv",
-    "numu_pr",
-    "numu_total",
-    "mu_conv",
-    "mu_pr",
-    "mu_total",
-    "nue_conv",
-    "nue_pr",
-    "nue_total",
-    "nutau_pr",
-]
-
-# Initialize empty grid
-for frac in all_component_names:
-    flux_def[frac] = np.zeros((len(mceq.e_grid), len(angles)))
-
-# fluxes calculated for different angles
-for ti, theta in enumerate(angles):
-    mceq.set_theta_deg(theta)
-    mceq.solve()
-
-    # same meaning of prefixes for muon neutrinos as for muons
-    flux_def["mu_conv"][:, ti] = mceq.get_solution("conv_mu+", mag) + mceq.get_solution(
-        "conv_mu-", mag
-    )
-
-    flux_def["mu_pr"][:, ti] = mceq.get_solution("pr_mu+", mag) + mceq.get_solution(
-        "pr_mu-", mag
-    )
-
-    flux_def["mu_total"][:, ti] = mceq.get_solution(
-        "total_mu+", mag
-    ) + mceq.get_solution("total_mu-", mag)
-
-    # same meaning of prefixes for muon neutrinos as for muons
-    flux_def["numu_conv"][:, ti] = mceq.get_solution(
-        "conv_numu", mag
-    ) + mceq.get_solution("conv_antinumu", mag)
-
-    flux_def["numu_pr"][:, ti] = mceq.get_solution("pr_numu", mag) + mceq.get_solution(
-        "pr_antinumu", mag
-    )
-
-    flux_def["numu_total"][:, ti] = mceq.get_solution(
-        "total_numu", mag
-    ) + mceq.get_solution("total_antinumu", mag)
-
-    # same meaning of prefixes for electron neutrinos as for muons
-    flux_def["nue_conv"][:, ti] = mceq.get_solution(
-        "conv_nue", mag
-    ) + mceq.get_solution("conv_antinue", mag)
-
-    flux_def["nue_pr"][:, ti] = mceq.get_solution("pr_nue", mag) + mceq.get_solution(
-        "pr_antinue", mag
-    )
-
-    flux_def["nue_total"][:, ti] = mceq.get_solution(
-        "total_nue", mag
-    ) + mceq.get_solution("total_antinue", mag)
-
-    # since there are no conventional tau neutrinos, prompt=total
-    flux_def["nutau_pr"][:, ti] = mceq.get_solution(
-        "total_nutau", mag
-    ) + mceq.get_solution("total_antinutau", mag)
-print("\U0001F973")
-
-
-## save the result
 with open(savepath, "wb") as f:
-    pickle.dump(((mceq.e_grid, angles), flux_def), f)
-
-# plotting can be found in background_flux.ipynb
+    dump(((egrid, zeniths), flux_grid), f)
---
+++ core/likelihood.py
@@ -0,0 +1,298 @@
+# This file contains a selection of functions that are used within
+# the Plenum notebooks
+import numpy as np
+from tools import array_source_interp
+from pathlib import Path
+from fluxes import astro_flux, atmo_background, flux_collection
+from aeff_calculations import calc_aeff_factor
+
+# get the baseline path of this project
+tmp_path = str(Path(__file__).parent.resolve())
+BASEPATH = "/".join(tmp_path.split("/")[:-1])
+
+
+
+def poisson_llh(mu_i, k_i):
+    """
+    Calculate the -2 log(Poisson Log-Likelihood) for given model parameters.
+
+    Parameters:
+    - mu_i (numpy.ndarray): Array of model predictions for each data point.
+    - k_i (numpy.ndarray): Array of observed counts for each data point.
+
+    Returns:
+    float: The -2 log(Poisson Log-Likelihood) value.
+
+    The Poisson Log-Likelihood is calculated using the formula:
+
+    L(data k | model mu) = prod_{i,j} mu_ij ** k_ij / k_ij! * exp(-mu_ij)
+
+    For numerical stability, the log of the Poisson probability is directly evaluated,
+    utilizing Stirling's approximation for the factorial function.
+
+    -2 log(L) = -2 [k_i log(mu_i) - mu_i - 0.5 log(2 pi k_i) + k_i - k_i log(k_i)]
+
+    Special cases are handled to avoid numerical issues:
+    - mu -> 0, k > 0      --> P -> 0
+    - k -> 0, mu > 0      --> P -> exp(-mu)
+    - k -> 0, mu -> 0     --> P -> 1
+
+    Note:
+    Since Asimov data can have floating-point values, this function is implemented
+    instead of using scipy.stats.poisson.logpmf, which fails for floats in k_i.
+
+    """
+    log_LLH = np.zeros_like(mu_i)
+    # k == 0, mu > 0:
+    _mask = (k_i == 0) & (mu_i > 0)
+    log_LLH[_mask] = -mu_i[_mask]
+    # k == 0, mu == 0:
+    _mask = (k_i == 0) & (mu_i == 0)
+    log_LLH[_mask] = 0
+    # k > 0, mu==0: should not happen! we'll assign a very negative value
+    _mask = (k_i > 0) & (mu_i == 0)
+    log_LLH[_mask] = -1e16
+    # k > 0, mu > 0
+    _mask = (k_i > 0) & (mu_i > 0)
+    log_LLH[_mask] = (
+        k_i[_mask] * np.log(mu_i[_mask])
+        - mu_i[_mask]
+        - 0.5 * np.log(2 * np.pi * k_i[_mask])
+        + k_i[_mask]
+        - k_i[_mask] * np.log(k_i[_mask])
+    )
+
+    return -2 * np.sum(log_LLH)
+
+# Set up LLH function
+# $ \mathcal{L}({\rm data}~k~ |~{\rm hypothesis}~\mu)
+#     = \prod_{{\rm bin\,}ij}^{N_{\rm bins}} \frac{\mu_{ij}^{k_{ij}}}{k_{ij}!}\cdot
+#     \exp \left( -\mu_{ij} \right)$
+
+
+# Background hypothesis $H_0(\mu = N_B)$ : only atmospheric neutrino flux
+
+# Signal hypothesis $H_1(\mu = \{N_B, N_S, \gamma\})$: atmospheric neutrino flux + astrophysical neutrino flux
+
+# Idea: data ($k$) are the perfect representation of our expectation; the hypothesis ($\mu$) is the model with the free parameters we'd like to know
+
+def ps_llh_single(
+    x,
+    aeff_factor_s,
+    aeff_factor_b,
+    bckg_flux,
+    k_i,
+    energy_resolution,
+    e_0,
+    phi_0,
+    shape,
+    verbose=False,
+):
+    """
+    Calculate the log-likelihood using Poisson statistics for a single dataset assuming consistent properties.
+
+    Parameters:
+        x (list): Fit parameters.
+            x[0]: Background normalization scaling.
+            x[1]: Signal normalization scaling.
+            x[2:]: Other signal parameters. See 'astro_flux' for further shapes and parameters.
+        aeff_factor_s (float): Effective area factor for the signal.
+        aeff_factor_b (float): Effective area factor for the background.
+        bckg_flux (list): Background flux values.
+        k_i (array-like): Observation/Asimov data.
+        energy_resolution (float): Energy resolution.
+        e_0 (float): Normalization energy. Default value is E0_NGC.
+        phi_0 (float): Normalization flux. Default value is PHI_NGC.
+        shape (str): Flux shape.
+        verbose (bool, optional): Whether to print additional information. Default is False.
+
+    Returns:
+        float: -2 * Log-likelihood value calculated using Poisson statistics. See 'poisson_llh'.
+
+    Note:
+        This function assumes that there is only one dataset with consistent properties.
+    """
+    # Calculate the background contribution
+    mu_b = (
+        atmo_background(
+            aeff_factor=aeff_factor_b,
+            bckg_vals=bckg_flux,
+            energy_resolution=energy_resolution,
+        )
+        * x[0]
+    )
+    # Calculate the signal contribution
+    mu_s = astro_flux(
+        aeff_factor_s,
+        10 ** aeff_factor_s.bin_mids[1],
+        energy_resolution,
+        x[1],
+        flux_collection[shape](
+            phi_0, *x[2:], e_0, shape
+        ),  # here we generate a flux tuple with the current parameters
+    )
+    if verbose:
+        # Print additional information if verbose mode is enabled
+        print(x[0], x[1], *x[2:])
+        print(flux_collection[shape](phi_0, *x[2:], e_0, shape))
+        print(np.sum(mu_b), np.sum(mu_s))
+
+    # Calculate the total expected events for the Poisson LLH
+    mu_i = mu_s + mu_b
+
+    # Calculate -2 * log-likelihood using Poisson statistics
+    return poisson_llh(mu_i, k_i)
+
+
+def ps_llh_multi(
+    x,
+    all_aeff_factor_s,
+    all_aeff_factor_b,
+    all_bckg_flux,
+    all_k,
+    all_eres,
+    shape,
+    e_0,
+    phi_0,
+):
+    """
+    Calculate the total log-likelihood across multiple datasets with different properties.
+
+    Parameters:
+        x (list): Fit parameters.
+        all_aeff_factor_s (list): List of effective area factors for the signal for each dataset.
+        all_aeff_factor_b (list): List of effective area factors for the background for each dataset.
+        all_bckg_flux (list): List of background flux values for each dataset.
+        all_k (list): List of observation/Asimov data for each dataset.
+        all_eres (list): List of energy resolutions for each dataset.
+        shape (str): Flux shape.
+        e_0 (float): Normalization energy.
+        phi_0 (float): Normalization flux.
+
+    Returns:
+        float: Total log-likelihood value across all datasets.
+
+    Note:
+        This function assumes that there are multiple datasets with different properties.
+    """
+    llh = 0
+    for i, aeffs in enumerate(all_aeff_factor_s):
+        llh += ps_llh_single(
+            x=x,
+            aeff_factor_s=aeffs,
+            aeff_factor_b=all_aeff_factor_b[i],
+            bckg_flux=all_bckg_flux[i],
+            k_i=all_k[i],
+            energy_resolution=all_eres[i],
+            e_0=e_0,
+            phi_0=phi_0,
+            shape=shape,
+        )
+    return llh
+
+
+def setup_multi_llh(
+    eres,
+    conf,
+    aeff_2d,
+    bckg_histo,
+    bg_config,
+    sig_config,
+    src_flux,
+    verbose=False,
+    return_s_b=False,
+):
+    """
+    Set up the components required for calculating the log-likelihood across multiple datasets.
+
+    Parameters:
+        eres (dict or array): Dictionary of arrays/mephistograms or single array/mephistogram representing the
+                                  energy resolutions for each dataset. If a dictionary is provided,
+                                  it should map dataset identifiers to their corresponding energy resolution.
+                                  If a float is provided, the same energy resolution will be used for all datasets.
+        conf (tuple): Tuple containing two lists - the first list represents dataset identifiers,
+                      and the second list contains corresponding scaling factors corresponding to
+                      either a lifetime scaling or effective-area scaling.
+        verbose (bool, optional): Whether to print additional information. Default is False.
+        return_s_b (bool, optional): Whether to return the individual signal and background histograms. Default is False.
+
+    Returns:
+        tuple: A tuple containing the following components for each dataset:
+            - all_aeff_factor_s (list): List of effective area factors for the signal.
+            - all_aeff_factor_b (list): List of effective area factors for the background.
+            - all_k (list): List of observation/Asimov data.
+            - all_bckg_flux (list): List of background flux values.
+            - if return_s_b: all_k_s, all_k_b (lists): List of signal and background histograms
+
+    Note:
+        This function assumes that the configuration parameters and required functions (e.g., `calc_aeff_factor`,
+        `atmo_background`, `astro_flux`, `array_source_interp`) are defined and accessible in the global namespace.
+        If `eres` is a dictionary, each dataset identifier should have an associated energy resolution value.
+        If `eres` is an  array/mephistogram, the same energy resolution will be used for all datasets.
+    """
+
+    all_aeff_factor_s = []
+    all_aeff_factor_b = []
+    all_k = []
+    all_bckg_flux = []
+    all_eres = []
+    if return_s_b:
+        all_k_s = []
+        all_k_b = []
+
+    for ident, factor in zip(*conf):
+        # Calculate effective area factors for background and signal
+        aeff_factor_bckg = calc_aeff_factor(aeff_2d[ident], **bg_config) * factor
+        aeff_factor_signal = calc_aeff_factor(aeff_2d[ident], **sig_config) * factor
+
+        # Calculate background flux
+        bckg_flux = array_source_interp(
+            bg_config["dec"],
+            bckg_histo[ident],
+            bckg_histo[ident].bin_mids[0],
+            axis=1,
+        )
+        # Determine the energy resolution for the current dataset
+        current_eres = eres[ident] if isinstance(all_eres, dict) else eres
+
+        # Calculate asimov data for atmospheric background
+        k_b = atmo_background(
+            aeff_factor=aeff_factor_bckg,
+            bckg_vals=bckg_flux,
+            energy_resolution=current_eres,
+        )
+
+        # Calculate asimov data for astrophysical signal with power law spectrum
+        k_s = astro_flux(
+            aeff_factor=aeff_factor_signal,
+            emids=10 ** aeff_factor_signal.bin_mids[1],
+            energy_resolution=current_eres,
+            phi_scaling=1,
+            flux_shape=src_flux,  # powerlaw
+        )
+
+        if verbose:
+            print("Asimov data sum:")
+            print("Background:", np.sum(k_b))
+            print("Signal:", np.sum(k_s))
+
+        all_aeff_factor_s.append(aeff_factor_signal)
+        all_aeff_factor_b.append(aeff_factor_bckg)
+        all_k.append(k_s + k_b)
+        all_bckg_flux.append(bckg_flux)
+        all_eres.append(current_eres)
+        if return_s_b:
+            all_k_s.append(k_s)
+            all_k_b.append(k_b)
+    if return_s_b:
+        return (
+            all_aeff_factor_s,
+            all_aeff_factor_b,
+            all_k,
+            all_bckg_flux,
+            all_eres,
+            all_k_b,
+            all_k_s,
+        )
+    else:
+        return all_aeff_factor_s, all_aeff_factor_b, all_k, all_bckg_flux, all_eres
